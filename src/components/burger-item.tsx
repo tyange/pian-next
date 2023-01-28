00c0b9bfbdad240f087ad7db6c8b5e2cf9c1a802
tyange <usun16@gmail.com>
2023-01-28 14:29:25 +0900

    burger item 스타일링

--- src/components/burger-item.tsx
+++ src/components/burger-item.tsx
@@ -5,19 +5,16 @@ type BurgerItemProps = {
 };
 const BurgerItem = ({ burger }: BurgerItemProps) => {
   return (
-    <li className="h-72 border rounded-lg shadow-md">
-      <div className="">
-        <div>
-          <span>버거 이름:</span>
-          <span>{burger.Name}</span>
+    <li className="border-2 border-gray-400 h-72">
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center w-5/6 p-5 border-b-2 border-gray-400 border-dashed">
+          <span className="text-3xl font-extrabold">{burger.Name}</span>
         </div>
-        <div>
-          <span>버거 브랜드:</span>
+        <div className="p-5">
           <span>{burger.Brand}</span>
         </div>
       </div>
-      <div className="">
-        <span>이 버거의 함의:</span>
+      <div className="p-5 font-serif italic">
         <p>{burger.Description}</p>
       </div>
     </li>
