5b0cda7888b9574f969ab2bc47f0801786ac5dd6
tyange <usun16@gmail.com>
2023-02-01 21:26:25 +0900

    Header에 Link 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import { IconUser } from "@tabler/icons-react";
 import { IconQuestionMark } from "@tabler/icons-react";
 import logoImg from "../../public/red-x-on-pickle.png";
-import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex justify-center py-10">
       <nav className="grid items-center w-5/12 grid-cols-5">
         <div className="w-20">
-          <Image src={logoImg} alt="로고 이미지" />
+          <Link href="/">
+            <Image src={logoImg} alt="로고 이미지" />
+          </Link>
         </div>
         <div>
           <ul className="flex gap-5"></ul>
@@ -18,9 +20,9 @@ const Header = () => {
         <div className="col-span-3 mr-10 justify-self-end">
           <ul className="flex gap-4">
             <li>
-              <a href="https://github.com/tyange">
+              <Link href="/auth">
                 <IconUser stroke={2} size={30} />
-              </a>
+              </Link>
             </li>
             <li>
               <Link href="">
